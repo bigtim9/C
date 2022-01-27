46724a8e3b20048ebfb6821a9e9b790284ac6ce2
bigtim9 <timmathias9@gmail.com>
2022-01-27 10:19:41 -0600

    Make file practive

---
+++ Practice/Practice.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int course_num;
+    course_num = 330;
+    printf("Hello World! \nWelcome to CS%d\n",course_num);
+    return 0;
+}
